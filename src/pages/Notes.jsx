79dc68af8f169ce4df1097df63a5a69efbc0374b
Modesto <mofesdo.dev@gmail.com>
2025-11-20 15:24:19 -0800

    Feat: Note component created and renders from notes.json

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
+import dummyNotes from "../dummy_notes";
+import {Link } from "react-router-dom";
+import { BsPlusLg } from "react-icons/bs";
+import NoteItem from "../components/NoteItem";
 
 function Notes() {
   return (
-    <div className="Notes">
-        Notes
-    </div>
+    <section>
+      <header className="notes__header">
+        <h2>My Notes</h2>
+        {/* <input type="text" autoFocus placeholder="Keyword..."/> */}
+        <button className="btn"><CiSearch/></button>
+      </header>
+      <div className="notes__container">
+        {dummyNotes.map(note => <NoteItem key={note.id} note={note}/>)}
+      </div>
+      <Link className="btn add__btn"><BsPlusLg/></Link>
+    </section>
   );
 }
 
