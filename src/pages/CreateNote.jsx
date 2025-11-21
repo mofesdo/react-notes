141a1feed06fd7739f6a78924c82434a3af67b52
Modesto <mofesdo.dev@gmail.com>
2025-11-20 16:02:10 -0800

    Feat: Edit Note route added + delete button

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,10 +1,19 @@
-import React from "react";  
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
 
 function CreateNote() {
   return (
-    <div className="CreateNote">
-        Create Note
-    </div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn"><IoIosArrowBack/></Link>
+        <button className="btn lg primary"> Save</button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" autoFocus placeholder="Title"/>
+        <textarea rows= "28" placeholder="Note details ..." name="textarea" id=""></textarea>
+      </form>
+    </section>
   );
 }
 
--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 function EditNote() {
   return (
-    <div className="EditNote">
-        Edit Note
-    </div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn"><IoIosArrowBack/></Link>
+        <button className="btn lg primary"> Save</button>
+        <button className="btn danger"> <RiDeleteBin6Line/></button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" autoFocus placeholder="Title"/>
+        <textarea rows= "28" placeholder="Note details ..." name="textarea" id=""></textarea>
+      </form>
+    </section>
   );
 }
+
 export default EditNote;
